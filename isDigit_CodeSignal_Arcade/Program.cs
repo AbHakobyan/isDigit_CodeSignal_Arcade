a70fb1a87e32083490a1f642895011440e5b2d5a
Albert Hakobyan <abhakobyan24@gmail.com>
2021-10-12 13:06:58 +0400

    Добавьте файлы проекта.

---
+++ isDigit_CodeSignal_Arcade/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isDigit_CodeSignal_Arcade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char v = '1';
+            bool x = isDigit(v);
+            Console.WriteLine(x);
+        }
+
+        static bool isDigit(char symbol)
+        {
+            int v = 0;
+            bool x;
+            return x = int.TryParse(symbol.ToString(),out v);
+        }
+    }
+}
